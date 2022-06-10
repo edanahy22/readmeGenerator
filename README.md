a2b9ff2214c15540dfc876c85a8b96a377734453
Elaine Danahy <edanahy22@gmail.com>
2022-06-10 11:58:51 -0500

    added code to generateMarkdown

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# readmeGenerator
+# test
+  ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+  ## Description 
+  test
+
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contribute)
+  *[Tests](#test)
+  *[Questions](#questions)
+
+  ## Installation
+  test
+
+  ## Usage
+  test
+
+  ## License 
+    This project is licensed under the MIT license.
+
+
--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your ReadMe?',
+        message: 'What is the title of your project?',
+    },
+    {
+        type:'input',
+        name: 'description',
+        message: 'Describe your project.'
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Describe how your app is installed.'
+        message: 'Describe how your project is installed.'
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Describe the usage of your app.'
+        message: 'Describe the usage of your project.' 
+       
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'What License are you using?'
+        message: 'Select a type of license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
     },
     {
         type: 'input',
         name: 'contribute',
-        message: 'Describe how others can contribute to your app.',
+        message: 'Describe how others can contribute to your project.',
     },
     {
         type: 'input',
@@ -39,19 +48,29 @@ const questions = [
         name: 'questions',
         message: 'How should others report questions or issues?',
     },
-    
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter yout GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email'
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+// function writeToFile(fileName, data) {
     
-}
+// }
 
 // TODO: Create a function to initialize app
 function init() {
-    promptUser(questions)
-    .then((answers) => fs.writeFileSync('README.md', writeToFile(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
+    inquirer
+    .prompt(questions)
+    .then((data) => fs.writeFileSync('README.md', generateMarkdown(data)))
+    .then(() => console.log('Successfully wrote to ReadMe'))
     .catch((err) => console.error(err));
 }
 
